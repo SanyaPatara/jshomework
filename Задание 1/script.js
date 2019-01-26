d040f9c8df6bd4720e3ecfbb723ad518c51db54b
SanyaPatara <sanya.patara@gmail.com>
2019-01-26 18:09:53 +0300

    homework's done

---
+++ Задание 1/script.js
@@ -0,0 +1,20 @@
+
+var newNum = +prompt('Input your number from 0 to 999');
+
+while (newNum > 999){
+    var newNum = +prompt('Input another number');
+
+}
+var newNum1 = newNum.toString();
+
+
+var number1 = {
+    hundreds: newNum1.charAt(0),
+    dozens: newNum1.charAt(1),
+    units: newNum1.charAt(2)
+};
+
+
+
+alert('hundreds: ' + number1.hundreds + ' dozens: ' + number1.dozens + ' units: ' + number1.units);
+
---
+++ Задание 2/script.js
@@ -0,0 +1,37 @@
+var product1 = {
+  price: 300,
+  color: 'red',
+  size: 'S'
+};
+var product2 = {
+    price: 500,
+    color: 'blue',
+    size: 'S'
+};
+var product3 = {
+    price: 450,
+    color: 'yellow',
+    size: 'M'
+};
+var product4 = {
+    price: 700,
+    color: 'yellow',
+    size: 'XS'
+};
+
+var prices = [product1.price, product2.price, product3.price, product4.price];
+
+function countBasketPrice(){
+    var summm = 0;
+
+    for (i = 0; i < prices.length; i++){
+        summm += prices[i];
+    }
+    return summm;
+}
+
+var cart = {
+    price: countBasketPrice()
+};
+
+console.log('Total price ' +  cart.price);
