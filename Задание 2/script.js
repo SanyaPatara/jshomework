9246dea4ce2db5f515301a0ccbd1a68d9dc0736b
SanyaPatara <sanya.patara@gmail.com>
2019-01-28 20:44:28 +0300

    Теперь работает решение с массивом объектов)

--- Задание 2/script.js
+++ Задание 2/script.js
@@ -1,32 +1,33 @@
-var product1 = {
+// Не знаю как обратиться к параметру прайс в объекте который лежим в массиве, чтобы сплюсовать все прайсы
+
+var prices = [
+product1 = {
   price: 300,
   color: 'red',
   size: 'S'
-};
-var product2 = {
+},
+product2 = {
     price: 500,
     color: 'blue',
     size: 'S'
-    
-};
-var product3 = {
+},
+product3 = {
     price: 450,
     color: 'yellow',
     size: 'M'
-};
-var product4 = {
+},
+product4 = {
     price: 700,
     color: 'yellow',
     size: 'XS'
-};
-
-var prices = [product1.price, product2.price, product3.price, product4.price];
+},
+];
 
 function countBasketPrice(){
     var summm = 0;
 
     for (i = 0; i < prices.length; i++){
-        summm += prices[i];
+        summm += prices[i].price;
     }
     return summm;
 }
